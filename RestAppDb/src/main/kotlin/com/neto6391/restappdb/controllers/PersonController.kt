119db9b58e81e1df4872222b255609a5f604095e
Neto6391 <neto6391@gmail.com>
2019-04-26 15:58:00 -0300

    Clean up class of Controller of RestAppDB

--- RestAppDb/src/main/kotlin/com/neto6391/restappdb/controllers/PersonController.kt
+++ RestAppDb/src/main/kotlin/com/neto6391/restappdb/controllers/PersonController.kt
@@ -1,58 +1,39 @@
 package com.neto6391.restappdb.controllers
 
 import com.neto6391.restappdb.models.Person
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import com.neto6391.restappdb.services.PersonServices
+import org.springframework.http.ResponseEntity
 
 @RestController
 @RequestMapping("/person")
 class PersonController (private val services: PersonServices) {
 
 
-	@RequestMapping(
-			method = [RequestMethod.GET],
-			produces = [MediaType.APPLICATION_JSON_VALUE]
-	)
+	@GetMapping
 	fun findAll():List<Person> {
 		return services.findAll()
 	}
 
-	@RequestMapping(
-			value = ["/{id}"],
-			method = [RequestMethod.GET],
-			produces = [MediaType.APPLICATION_JSON_VALUE]
-	)
+	@GetMapping("/{id}")
 	fun findById(@PathVariable(value="id")  id:Long): Person {
 			return services.findById(id)
 	}
 
-	@RequestMapping(
-			method = [RequestMethod.POST],
-			consumes = [MediaType.APPLICATION_JSON_VALUE],
-			produces = [MediaType.APPLICATION_JSON_VALUE]
-	)
+	@PostMapping
 	fun create(@RequestBody person: Person): Person {
 		return services.create(person)
 	}
 
-	@RequestMapping(
-			method = [RequestMethod.PUT],
-			consumes = [MediaType.APPLICATION_JSON_VALUE],
-			produces = [MediaType.APPLICATION_JSON_VALUE]
-	)
+	@PutMapping
 	fun update(@RequestBody person: Person): Person {
 		return services.update(person)
 	}
 
-	@RequestMapping(
-			value = ["/{id}"],
-			method = [RequestMethod.DELETE]
-	)
-	fun delete(@PathVariable(value="id")  id:Long) {
+	@DeleteMapping("/{id}")
+	fun delete(@PathVariable(value="id")  id:Long):ResponseEntity<Person> {
 		services.delete(id)
+		return ResponseEntity.ok().build()
 	}
 
 }
