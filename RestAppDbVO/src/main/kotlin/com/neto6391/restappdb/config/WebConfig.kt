72bc139268cd45e495c3be4c950926e07856d12c
Neto6391 <neto6391@gmail.com>
2019-05-06 17:17:01 -0300

    Change Configuration Method for many different JSON/XML

--- RestAppDbVO/src/main/kotlin/com/neto6391/restappdb/config/WebConfig.kt
+++ RestAppDbVO/src/main/kotlin/com/neto6391/restappdb/config/WebConfig.kt
@@ -12,11 +12,41 @@ class WebConfig : WebMvcConfigurer {
 
     //Override Method for permit content Negotiation of files JSON/XML
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
-        configurer.favorParameter(false)
+        /*
+        *
+        * For use Extension(.json/.xml) example url /person.extension
+        * */
+//      configurer.favorParameter(false)
+//                .ignoreAcceptHeader(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML)
+
+         /*
+         *
+         * For use Query Param by type('json'/'xml') example url /person/?mediaType='type'
+         * */
+//       configurer.favorPathExtension(false)
+//                .favorParameter(true)
+//                .parameterName("mediaType")
+//                .ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML)
+        /*
+        *
+        * For use Header example in Header request by type(json/xml) Send "{Accept: 'application/type'}"
+        * */
+        configurer.favorPathExtension(false)
+                .favorParameter(false)
                 .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
     }
+
+
 
 }
