06ac2d673980a51943b6a3af35d692c6625d48a2
Neto6391 <Neto6391@gmail.com>
2019-05-29 15:38:01 -0300

    Create App Management Users MS

---
+++ microservices/PhotoApiAccountManagement/src/main/kotlin/com/appsdeveloperblog/photoapp/api/account/PhotoApiAccountManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.photoapp.api.account
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhotoApiAccountManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhotoApiAccountManagementApplication>(*args)
+}
