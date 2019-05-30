fb00f002ebaa7f3eb797a8a7f1ae7731fc4273b7
Neto6391 <Neto6391@gmail.com>
2019-05-30 15:37:28 -0300

    Add Feature Public Id

--- microservices/PhotoAppApiUsers/src/main/kotlin/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.kt
+++ microservices/PhotoAppApiUsers/src/main/kotlin/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.kt
@@ -1,10 +1,14 @@
 package com.appsdeveloperblog.photoapp.api.users.service
 
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto
+import java.util.*
 
 class UsersServiceImpl : UsersService {
-    override fun createUsers(userDetails: UserDto): UserDto {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun createUsers(userDetails: UserDto): UserDto? {
+        //Generate Random Unique User Id
+        userDetails.setId(UUID.randomUUID().toString())
+
+        return null
     }
 
 }
