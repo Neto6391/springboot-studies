3fa0195251b9fbd769f08ddec0a71794d229a50b
Neto6391 <Neto6391@gmail.com>
2019-05-30 15:54:59 -0300

    Add Feature Entity Users

---
+++ microservices/PhotoAppApiUsers/src/main/kotlin/com/appsdeveloperblog/photoapp/api/users/data/UserEntity.kt
@@ -0,0 +1,42 @@
+package com.appsdeveloperblog.photoapp.api.users.data
+
+import java.io.Serializable
+import javax.persistence.*
+
+@Entity
+@Table(name="users")
+data class UserEntity(
+        @Id
+        @GeneratedValue
+        private var id:Long = 0L,
+
+        @Column(nullable = false, length=50)
+        private var firstName:String = "",
+
+        @Column(nullable = false, length=50)
+        private var lastName:String = "",
+
+        @Column(nullable = false, length=120, unique = true)
+        private var email:String = "",
+
+        @Column(nullable = false, unique = true)
+        private var userId:String,
+
+        @Column(nullable = false, unique = true)
+        private var encryptedPassword: String
+) : Serializable {
+
+    //Getters
+    fun getFirstName():String { return firstName }
+    fun getLastName():String { return lastName}
+    fun getEmail():String { return email }
+    fun getId():String { return userId}
+    fun getEncriptedPassword():String { return encryptedPassword }
+
+    //Setters
+    fun setFirstName(firstName:String) { this.firstName = firstName }
+    fun setLastName(lastName:String) { this.lastName = lastName }
+    fun setEmail(email:String) { this.email = email }
+    fun setId(id:String) { this.userId = id }
+    fun setEncriptedPassword(encryptedPassword: String) { this.encryptedPassword = encryptedPassword }
+}
