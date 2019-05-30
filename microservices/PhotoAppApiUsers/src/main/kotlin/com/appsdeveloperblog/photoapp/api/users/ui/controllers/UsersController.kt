1035a93cafd35d8a2ee3a3a51af3c8de2f9b72e9
Neto6391 <Neto6391@gmail.com>
2019-05-30 17:33:51 -0300

    Implement Test Controller

--- microservices/PhotoAppApiUsers/src/main/kotlin/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.kt
+++ microservices/PhotoAppApiUsers/src/main/kotlin/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.kt
@@ -1,6 +1,10 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers
 
+import com.appsdeveloperblog.photoapp.api.users.service.UsersService
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto
 import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel
+import org.modelmapper.ModelMapper
+import org.modelmapper.convention.MatchingStrategies
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.*
@@ -13,13 +17,23 @@ class UsersController {
     @Autowired
     private lateinit var env:Environment
 
+    @Autowired
+    private lateinit var usersService: UsersService
+
     @GetMapping("/status/check")
     fun status():String {
         return "Working on port ${env.getProperty("local.server.port")}"
     }
 
     @PostMapping
-    fun createUser(@Valid @RequestBody users:CreateUserRequestModel):String {
+    fun createUser(@Valid @RequestBody usersDetails:CreateUserRequestModel):String {
+
+        val modelMapper = ModelMapper()
+        modelMapper.configuration.matchingStrategy = MatchingStrategies.STRICT
+
+        val userDto = modelMapper.map(usersDetails, UserDto::class.java)
+        usersService.createUsers(userDto)
+
         return "Create user method is called"
     }
 
