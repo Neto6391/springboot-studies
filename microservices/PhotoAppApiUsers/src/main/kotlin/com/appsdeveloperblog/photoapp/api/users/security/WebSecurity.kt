138e9cf32faf0bf73e5799d697c531616b86e711
Neto6391 <Neto6391@gmail.com>
2019-05-30 23:56:49 -0300

    Refactor WebSecurity to support only Zuul Ip Address and Add Feature MediaTypes JSON/XML

--- microservices/PhotoAppApiUsers/src/main/kotlin/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.kt
+++ microservices/PhotoAppApiUsers/src/main/kotlin/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.kt
@@ -1,7 +1,9 @@
 package com.appsdeveloperblog.photoapp.api.users.security
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -9,7 +11,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
 @EnableWebSecurity
-class WebSecurity : WebSecurityConfigurerAdapter() {
+class WebSecurity(
+        @Autowired private var environment: Environment
+) : WebSecurityConfigurerAdapter() {
 
 
     @Bean
@@ -22,7 +26,7 @@ class WebSecurity : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/users/**").permitAll()
+                .authorizeRequests().antMatchers("/users/**").hasIpAddress(environment.getProperty("gateway.ip"))
                 .and()
                 .headers().frameOptions().disable()
 
--- microservices/PhotoAppApiUsers/src/main/kotlin/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.kt
+++ microservices/PhotoAppApiUsers/src/main/kotlin/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.kt
@@ -9,6 +9,7 @@ import org.modelmapper.convention.MatchingStrategies
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
@@ -28,7 +29,10 @@ class UsersController {
         return "Working on port ${env.getProperty("local.server.port")}"
     }
 
-    @PostMapping
+    @PostMapping(
+            consumes = [MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE]
+    )
     fun createUser(@Valid @RequestBody usersDetails:CreateUserRequestModel):ResponseEntity<CreateUserResponseModel> {
 
         val modelMapper = ModelMapper()
